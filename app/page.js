f8369d41706b7d79ad841720066884f2f9edf770
surya <suryamgowda93@gmail.com>
2025-10-21 11:04:37 +0530

    done with the conact form

--- app/page.js
+++ app/page.js
@@ -1,16 +1,20 @@
+// app/page.js or pages/index.js (depending on your structure)
 import Navbar from "@/components/Navbar";
 import Header from "@/components/Header";
 import About from "@/components/About";
 import Services from "@/components/Services";
 import Work from "@/components/Work";
+import Contact from "@/components/Contact";
+
 export default function Home() {
   return (
     <>
-      <Navbar/>
-      <Header/>
-      <About/>
-      <Services/>
-      <Work/>
+      <Navbar />
+      <Header />
+      <About />
+      <Services />
+      <Work />
+      <Contact />
     </>
   );
 }
